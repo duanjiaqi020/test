6cd05773765d3d658f54cbda5e9c373484847201
duanjiaqi020 <759177632@qq.com>
2020-06-19 19:27:37 +0800

    321321

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,6 +2,8 @@
   <div class="login_bk">
     <div class="login_top">
       <img class="login_top_img" src="..\assets\login_bg.png" alt />
+    </div>
+    <div class="login_top_text0">
       <div class="login_top_text">登录</div>
       <div class="login_top_text1">welcome back</div>
     </div>
@@ -15,44 +17,79 @@
       </div>
       <div class="login_mid_user">
         <div class="login_mid_username">密码</div>
+
         <div class="login_mid_username1">
           <input class="login_mid_border" type="password" placeholder="请输入密码" />
         </div>
       </div>
-      <div class="login_mid_denglu">
-        <div class="login_mid_button" button="登录">登录
+      <div class="login_mid_user">
+        <div class="login_mid_username">验证码</div>
+
+        <div class="login_mid_username1">
+          <input class="login_mid_border" type="text" placeholder="请输入验证码" />
+          <div class="code" @click="getCode" v-show="show">点击获取验证码</div>
+          <div class="code" v-show="!show">{{count}}s</div>
         </div>
+      </div>
+
+      <div class="login_mid_denglu">
+        <div class="login_mid_button" button="登录">登录</div>
       </div>
     </div>
   </div>
 </template>
 <script>
 export default {
-  name: "login"
+  data() {
+    return {
+      show: true,
+      code: "",
+      count: "",
+      timer: null
+    };
+  },
+  methods: {
+    getCode() {
+      let codeTime = 5;
+      if (this.show) {
+        this.show = false;
+        this.count = codeTime;
+        this.timer = setInterval(() => {
+          if (this.count > 0) {
+            this.count--;
+          } else {
+            clearInterval(this.timer);
+            this.show = true;
+          }
+        }, 1000);
+      }
+    }
+  }
 };
 </script>
 <style>
-.login_top {
-  height: 246px;
-}
 .login_top_img {
+  border-bottom: 1px dashed #4fc3f1;
   position: absolute;
   width: 100%;
-  border-bottom: 1px dashed #4fc3f1;
+}
+.login_top_text0 {
+  color: #ffffff;
+  padding-top: 104px;
+  position: relative;
 }
 .login_top_text {
-  position: relative;
-  padding-top: 104px;
   margin-left: 34px;
   font-size: 30px;
-  color: #ffffff;
 }
 .login_top_text1 {
-  position: relative;
   padding-top: 1px;
   margin-left: 36px;
   font-size: 25px;
-  color: #ffffff;
+}
+.login_mid {
+  position: relative;
+  margin-top: 87px;
 }
 
 .login_mid_username {
@@ -66,7 +103,7 @@ export default {
   border-bottom: 1px solid #bfbfbf;
   margin-left: 38px;
   margin-right: 38px;
- 
+  position: relative;
 }
 .login_mid_button {
   width: 300px;
@@ -82,14 +119,30 @@ export default {
   box-sizing: border-box;
 }
 .login_mid_border {
-     border: none;
-    font-size: 16px;
-    color: #aeaeae;
-    width: 100%;
-    height: 40px;
-    line-height: 40px;
-    outline-style: none ;
+  border: none;
+  font-size: 16px;
+  color: #aeaeae;
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+  outline-style: none;
 }
-
-
+.login_mid_denglu {
+  vertical-align: middle;
+}
+.clear {
+  clear: both;
+}
+.login_mid_yz {
+  text-align: center;
+}
+.login_mid_input {
+  border-radius: 25px;
+  box-sizing: border-box;
+}
+.code {
+  position: absolute;
+  right: 10px;
+  top: 10px;
+}
 </style>
